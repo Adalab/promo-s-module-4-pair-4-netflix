6403433cbab1769a8588b681819fc421c6640ea3
Maria <araujomaria84@gmail.com>
2023-03-31 17:54:16 +0200

    Changes src/index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,63 @@
+//crear y configurar el servidor.
+
+//imprtamos todos los módulos npm necesarios: npm install express, cors, mysql2
 const express = require('express');
 const cors = require('cors');
 
-// create and config server
+// Creamos el servidor
 const server = express();
-server.use(cors());
-server.use(express.json());
 
-// init express aplication
+// Configuramos el servidor //apuntes intro
+server.use(cors());
+server.use(express.json({limit: '25mb'}));
+
+// Arrancamos el servidor en el puerto 4000
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+//conectar a la base de datos //apuntes mysql+express
+const mysql = require('mysql2/promise');
+
+mysql
+  .createConnection({
+    host: 'localhost',
+    database: 'mydb',
+    user: 'root',
+    password: 'Notita28',
+  })
+  .then(connection => {
+    connection
+      .connect()
+      .then(() => {
+        console.log('Conectado con el identificador ' + connection.threadId);
+      })
+      .catch((err) => {
+        console.error('Error de conexion: ' + err.stack);
+      });
+  })
+  .catch((err) => {
+    console.error('Error de configuración: ' + err.stack);
+  });
+
+  //Ahora, tenemos que incorporar el código para lanzar la sentencia select dentro de una función de un endpoint. Añade al final del fichero index.js.
+  //en los apuntes pone app.get, pero nosotras la hemos llamado server
+  server.get('/movies', (req, res) => {
+    console.log('Pidiendo a la base de datos información de las películas.');
+    connection
+      .query('SELECT * FROM movies')
+      .then(([results, fields]) => {
+        console.log('Información recuperada:');
+        results.forEach((result) => {
+          console.log(result);
+        });
+        res.json({
+          success: true,
+          movies:  moviesListDatabases
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -9,9 +9,13 @@
 const getMoviesFromApi = () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('http://localhost:4000/movies')
+  return fetch("http://localhost:4000/movies", {
+    method:"GET",
+    headers:{"Content-Type": "application/json"}, //proyecto grupo
+  })
     .then(response => response.json())
     .then(data => {
+  //CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
       return data;
     });
 };
