eb84210aa3a743ae1b4c9b61d39bc1cb6736db8e
Maria <araujomaria84@gmail.com>
2023-03-31 11:21:41 +0200

    Express JS I try

--- bd/netflix/codigo tabla usuaria-pelicula.sql
+++ bd/netflix/codigo tabla usuaria-pelicula.sql
@@ -2,3 +2,10 @@ use mydb;
 select * from rel_movies_users;
 
 INSERT INTO rel_movies_users (fkUser, fkMovie) VALUES (1, 1), (1, 3), (2, 3);
+
+
+-- VER QUÉ COLUMNAS COINCIDEN EN LA TABLA
+select actors.name, movies.title
+from actors INNER JOIN rel_movies_actors ON actors.idactors = rel_movies_actors.fkActor
+INNER JOIN movies ON movies.idmovies = rel_movies_actors.fkMovie;
+
--- web/src/index.js
+++ web/src/index.js
@@ -4,6 +4,28 @@ import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import './stylesheets/index.scss';
 
+const mysql = require('mysql2/promise');
+
+mysql
+  .createConnection({
+    host: 'localhost',
+    database: 'mydb',
+    user: 'root',
+    password: 'Notita28',
+  })
+  .then(connection => {
+    connection
+      .connect()
+      .then(() => {
+        console.log('Conectado con el identificador ' + connection.threadId);
+      })
+      .catch((err) => {
+        console.error('Error de conexion: ' + err.stack);
+      });
+  })
+  .catch((err) => {
+    console.error('Error de configuración: ' + err.stack);
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +36,19 @@ root.render(
   </React.StrictMode>
 );
 
+app.get('/user', (req, res) => {
+  console.log('Pidiendo a la base de datos información de los empleados.');
+  connection
+    .query('SELECT * FROM empleados')
+    .then(([results, fields]) => {
+      console.log('Información recuperada:');
+      results.forEach((result) => {
+        console.log(result);
+      });
+
+      res.json(results);
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
